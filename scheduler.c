d5bdb05a5e55e7db09dd391b39b9c5728070b167
yousefwmorsy <166176410+yousefwmorsy@users.noreply.github.com>
2025-04-29 01:37:38 +0300

    Update scheduler.c

--- scheduler.c
+++ scheduler.c
@@ -5,17 +5,22 @@ float WTA; // waiting time of the process
 int WT; // turnaround time of the process
 int execTime; // execution time of the process
 int countGlobal = 0; // number of processes in the system
+PCBPriQ* PriQ; //ready queue for HPF
+
+
+
+
 
 bool readyQNotEmpty(int algo){
     switch (algo){
         case HPF:
-
+            return !(PCBPriQ_isEmpty(PriQ));
             break;
         case SRTN:
-
+            
             break;
         case RR:
-
+            
             break;
     }
 }
@@ -66,36 +71,47 @@ void schedulerPrefPrint(FILE *fp){ //missing std WTA I am to tired to do it now
 
 bool checkifEnd(int msg_q){
     ProcessMsg msg;
-    if(msgrcv(msg_q, &msg, sizeof(ProcessMsg), 2, IPC_NOWAIT)){ //wait for msg of type 2
+    //printf("Checked if end\n");
+    if(msgrcv(msg_q, &msg, sizeof(ProcessMsg) - sizeof(long), 2, IPC_NOWAIT)!=-1){ //wait for msg of type 2
+        //printf("Returned true \n");
         return true;
     }
+    //printf("returned false \n");
     return false;
 }
+
 void checkforNewProcesses(int msg_q, int algo){
     ProcessMsg msg;
-    while(msgrcv(msg_q, &msg, sizeof(ProcessMsg), 1, IPC_NOWAIT)){ //wait for msg of type 1 (process)
+    //printf("Checking for new processes...\n");
+    while(msgrcv(msg_q, &msg, sizeof(ProcessMsg) - sizeof(long), 1, IPC_NOWAIT)!=-1){ //wait for msg of type 1 (process)
+        printf("Received process message: ID=%d, Arrival=%d, Runtime=%d, Priority=%d\n", msg.id, msg.arrivalTime, msg.runTime, msg.priority);
         int pid = fork();
         if(pid==-1){
             perror("Process fork failed\n");
             return ;
         }
         else if(pid == 0){
-            execl("Compiled/process.out","./Compiled/process.out", (char*)NULL);
+            // Use absolute path for process.out to avoid path issues
+            execl("./Compiled/process.out","process.out", (char*)NULL);
             perror("Process execl failed\n");
-            return;
+            exit(1);
         }
         else {
+            printf("Forked process with PID=%d\n", pid);
+            kill(pid, SIGSTOP);
             pcb *obj = malloc(sizeof(pcb)); 
             if (obj == NULL) {
                 perror("Memory allocation for PCB failed\n");
                 return;
             }
-            *obj = pcb_init(&msg, pid); // Initialize the PCB
+            pcb tempPcb = pcb_init(&msg, pid);
+            memcpy(obj, &tempPcb, sizeof(pcb)); 
 
             //add to suitable ds
             switch(algo){
                 case HPF:
-
+                    PCBPriQ_enqueue(PriQ, obj);
+                    printf("Enqueued process ID=%d into priority queue\n", obj->givenid);
                     break;
                 case SRTN: 
                     
@@ -130,6 +146,35 @@ void SRTN (int num_process, pcb *process[]){
         if(curr_process->remainingTime == 0){/*Not inserting in the Queue and send to the schedular that the process finish its work*/}
         else{ PriQueue_insert(PriQueue_obj, curr_process);}        
         sleep(1);
+    }
+
+}
+
+void HPF_Iter(){
+    pcb* head = PCBPriQ_peek(PriQ);
+    if (head == NULL) {
+        //printf("No process in the queue to run\n");
+        return;
+    }
+    
+    if(head->remainingTime == 0){
+        printf("Process %d Terminated at %d\n", head->givenid, getClk());
+        kill(head->sysstemid, SIGKILL); //temporary for testing purposes
+        PCBPriQ_dequeue(PriQ);
+    }
+    else{
+        // Set waiting time when first executing the process
+        if(head->waitingTime == -1){
+            head->waitingTime = getClk() - head->arrivalTime;
+
+        }
+        // Continue the process execution
+        kill(head->sysstemid, SIGCONT);
+        
+        // Only decrement the remaining time once per clock tick
+        head->remainingTime--;
+        head->status = RUNNING;
+        printf("Running Process %d at %d, remaining time: %d\n", head->givenid, getClk(), head->remainingTime);
     }
 
 }
@@ -212,14 +257,35 @@ int main(int argc, char *argv[])
     
     enum schedulealgo algo = atoi(argv[1]);
     int quantum = atoi(argv[2]);
+    printf("Algo no %d, quantum=%d\n", algo, quantum);
     int MessageQueueId = msgget(MSG_KEY, IPC_CREAT | 0666);
-
+    if (MessageQueueId == -1) {
+        perror("Failed to create/get message queue");
+        exit(1);
+    }
+    printf("Message queue ID (sch): %d\n", MessageQueueId);
+    
+    //initialise queues to be used in algorithm iterations
+    PriQ = PCBPriQ_init(); 
+    printf("PCB init completed\n");
+    
+    
+    int lastClk = -1;
+    while(!checkifEnd(MessageQueueId) || readyQNotEmpty(algo)) { 
+        // Check for new processes only when needed
+        if(!checkifEnd(MessageQueueId)) {
+            checkforNewProcesses(MessageQueueId, algo);
+        }
 
      while(readyQNotEmpty(algo)){ //checks that the readyqueue is not empty (not all processes terminated)
         if(!checkifEnd(MessageQueueId)){ //checks for ending msg from generator indicating no more processes
             checkforNewProcesses(MessageQueueId, algo); //checks for new processes sent by gen and adds them to ready queue
         }
-
+        // Only run algorithm iterations when the clock ticks
+        int currentClk = getClk();
+        if (currentClk > lastClk) {
+            lastClk = currentClk;
+        }
         //run the algorithms (one iteration)
         switch (algo){
             case HPF:
